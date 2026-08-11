8f07ffdfeda8fb9e104beb21eb757212016d01ea
rafael-franque <ilovememorias@gmail.com>
2026-08-11 15:19:11 -0300

    v0.9.1 - implementando esse objeto pedidoAtual

--- script.js
+++ script.js
@@ -1,4 +1,89 @@
+//let arquivoSelecionado = null;
+// ==========================================
+// Configurações do Pedido
+// ==========================================
+
+const MAX_FOTOS = 5;
+
 let arquivoSelecionado = null;
+
+let pedidoAtual = JSON.parse(
+    localStorage.getItem("fotoImaPedido")
+) || null;
+
+
+// ==========================================
+// Criar novo pedido
+// ==========================================
+
+function criarPedido(nome){
+
+    const agora = new Date();
+
+    const pedido = {
+
+        id:
+            "PED-" +
+            agora.getFullYear() +
+            String(agora.getMonth()+1).padStart(2,"0") +
+            String(agora.getDate()).padStart(2,"0") +
+            "-" +
+            agora.getHours() +
+            agora.getMinutes() +
+            agora.getSeconds() +
+            "-" +
+            Math.random()
+                .toString(36)
+                .substring(2,6)
+                .toUpperCase(),
+
+        nome: nome,
+
+        fotos: 0
+
+    };
+
+    pedidoAtual = pedido;
+
+    salvarPedido();
+
+    return pedido;
+
+}
+
+
+// ==========================================
+// Salvar Pedido
+// ==========================================
+
+function salvarPedido(){
+
+    localStorage.setItem(
+
+        "fotoImaPedido",
+
+        JSON.stringify(pedidoAtual)
+
+    );
+
+}
+
+
+// ==========================================
+// Finalizar Pedido
+// ==========================================
+
+function finalizarPedido(){
+
+    pedidoAtual = null;
+
+    localStorage.removeItem(
+
+        "fotoImaPedido"
+
+    );
+
+}
 
 // COLE AQUI A URL /exec DO APPS SCRIPT
 const API_URL = "https://foto-ima-api.ilovememorias.workers.dev/";
